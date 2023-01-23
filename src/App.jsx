c1caae40ba95e565480624fcf84ece61d79ac2ef
qu3bin <kevinguillendouglas@gmail.com>
2023-01-23 17:47:50 -0300

    Article modified

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
       <article className="notification unread">
         <img className="avatar" src={avatar1} alt="" />
         <div>
-          <p className="detail dot"><span className="user">Mark Webber</span> reacted to your recent post <a className="link">My first tournament today!</a></p>
+          <h2 className="detail dot"><span className="user">Mark Webber</span> reacted to your recent post <a className="link">My first tournament today!</a></h2>
           <p className="ago">1m ago</p>
         </div>
       </article>
@@ -46,7 +46,7 @@ function App() {
       <article className="notification unread">
         <img className="avatar" src={avatar2} alt="" />
         <div>
-          <p className="detail dot"><span className="user">Angela Gray</span> followed you</p>
+          <h2 className="detail dot"><span className="user">Angela Gray</span> followed you</h2>
           <p className="ago">5m ago</p>
         </div>
       </article>
@@ -54,7 +54,7 @@ function App() {
       <article className="notification unread">
         <img className="avatar" src={avatar3} alt="" />
         <div>
-          <p className="detail dot"><span className="user">Jacob Thompson</span> has joined your group <a className="link">Chess Club</a></p>
+          <h2 className="detail dot"><span className="user">Jacob Thompson</span> has joined your group <a className="link">Chess Club</a></h2>
           <p className="ago">1 day ago</p>
         </div>
       </article>
@@ -62,7 +62,7 @@ function App() {
       <article className="notification">
         <img className="avatar" src={avatar4} alt="" />
         <div>
-          <p className="detail"><span className="user">Rizky Hasanuddin</span> sent you a private message</p>
+          <h2 className="detail"><span className="user">Rizky Hasanuddin</span> sent you a private message</h2>
           <p className="ago">5 days ago</p>
           <p className="msg">Hello, thanks for setting up the Chess Club. 
           I've been a member for a few weeks now and I'm 
@@ -73,7 +73,7 @@ function App() {
       <article className="notification justify">
         <img className="avatar" src={avatar5} alt="" />
         <div>
-          <p className="detail"><span className="user">Kimberly Smith</span> commented on your picture</p>
+          <h2 className="detail"><span className="user">Kimberly Smith</span> commented on your picture</h2>
           <p className="ago">1 week ago</p>
         </div>
         <div className="pic-container">
@@ -84,7 +84,7 @@ function App() {
       <article className="notification">
         <img className="avatar" src={avatar6} alt="" />
         <div>
-          <p className="detail"><span className="user">Nathan Peterson</span> reacted to your recent post <a className="link">5 end-game strategies to increase your win rate</a></p>
+          <h2 className="detail"><span className="user">Nathan Peterson</span> reacted to your recent post <a className="link">5 end-game strategies to increase your win rate</a></h2>
           <p className="ago">2 weeks ago</p>
         </div>
       </article>
@@ -92,7 +92,7 @@ function App() {
       <article className="notification">
         <img className="avatar" src={avatar7} alt="" />
         <div>
-          <p className="detail"><span className="user">Anna Kim</span> left the group <a className="link">Chess Club</a></p>
+          <h2 className="detail"><span className="user">Anna Kim</span> left the group <a className="link">Chess Club</a></h2>
           <p className="ago">2 weeks ago</p>
         </div>
       </article>
